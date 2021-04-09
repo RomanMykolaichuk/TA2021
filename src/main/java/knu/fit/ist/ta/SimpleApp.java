0f3cce9aaa666942c89d9f145566eb5d28f7f4a9
Roman Mykolaichuk <mykroman@ukr.net>
2021-04-09 09:26:41 +0300

    lab1

--- src/main/java/knu/fit/ist/ta/SimpleApp.java
+++ src/main/java/knu/fit/ist/ta/SimpleApp.java
@@ -5,24 +5,38 @@
  */
 package knu.fit.ist.ta;
 
+import static java.lang.Math.PI;
+import java.text.DecimalFormat;
+
 /**
  *
  * @author 38068
  */
 public class SimpleApp {
-    
-    
-    /* public static void main(String[] args) {
-    MyFirstJavaClass mFJC = new MyFirstJavaClass(5);
-    
-    System.out.println(mFJC.toString());
-    
-    MyFirstJavaClass.setMyStaticInt(10);
-    
-    System.out.println(MyFirstJavaClass.toStringStatic());
-    
-    }*/
-    
+
+    public static void main(String[] args) {
+        /* MyFirstJavaClass mFJC = new MyFirstJavaClass(5);
+        
+        System.out.println(mFJC.toString());
+        
+        MyFirstJavaClass.setMyStaticInt(10);
+        
+        System.out.println(MyFirstJavaClass.toStringStatic());*/
+
+        /* DecimalFormat df = new DecimalFormat("###.##");
+        System.out.println(df.format(PI));*/
+        
+        float f1 = 2.035678f;
+        
+        float f2 = 2.056587f;
+        
+        System.out.println(Math.abs(f1-f2)<=0.01f);
+        
+       
+        
+        
+        
+    }
     // comment
-    
+
 }
